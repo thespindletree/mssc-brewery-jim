49c6aafd0c0e5c55da136b89cf4f1ef4e75efef0
Jim C <james.c@argodevops.co.uk>
2022-10-03 16:20:57 +0100

    End of section 78- Mapstruct config in POM

---
+++ target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,54 @@
+package guru.springframework.msscbrewery.web.mappers;
+
+import guru.springframework.msscbrewery.domain.Beer;
+import guru.springframework.msscbrewery.domain.Beer.BeerBuilder;
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import guru.springframework.msscbrewery.web.model.BeerDto.BeerDtoBuilder;
+import guru.springframework.msscbrewery.web.model.v2.BeerStyleEnum;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-10-03T16:20:23+0100",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.4 (Private Build)"
+)
+@Component
+public class BeerMapperImpl implements BeerMapper {
+
+    @Override
+    public BeerDto beerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDtoBuilder beerDto = BeerDto.builder();
+
+        beerDto.id( beer.getId() );
+        beerDto.beerName( beer.getBeerName() );
+        if ( beer.getBeerStyle() != null ) {
+            beerDto.beerStyle( beer.getBeerStyle().name() );
+        }
+        beerDto.upc( beer.getUpc() );
+
+        return beerDto.build();
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        BeerBuilder beer = Beer.builder();
+
+        beer.id( dto.getId() );
+        beer.beerName( dto.getBeerName() );
+        if ( dto.getBeerStyle() != null ) {
+            beer.beerStyle( Enum.valueOf( BeerStyleEnum.class, dto.getBeerStyle() ) );
+        }
+        beer.upc( dto.getUpc() );
+
+        return beer.build();
+    }
+}
